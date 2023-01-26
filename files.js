e51aa1fa9e66455e68f66c3e596b582c10103503
Topaz Bar <topaz1008@gmail.com>
2023-01-26 18:09:33 +0200

    chore(): add script to update submodules

---
+++ files.js
@@ -0,0 +1,58 @@
+export const files = {
+    'canvas-fluid-solver': {
+        js: [
+            'canvas-fluid-solver/vendors/dat.gui.min.js',
+            'canvas-fluid-solver/src/fluidsolver.js',
+            'canvas-fluid-solver/src/main.js',
+        ],
+        css: ['canvas-fluid-solver/src/style.css']
+    },
+    'canvas-node-garden': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    },
+    'canvas-particle-system': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    },
+    'canvas-raytracer': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    },
+    'double-pendulum': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    },
+    'voronoi-breakable': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    },
+    'webgl-julia-set': {
+        js: [
+            '',
+            '',
+            '',
+        ],
+        css: ['']
+    }
+};
---
+++ submodules-update.ps1
@@ -0,0 +1,7 @@
+# Pull updated version from original repo
+git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
+
+# Make sure we update npm packages
+git submodule foreach --recursive npm update
